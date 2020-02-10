41ba293c27b6e73588b788a5251ffbd33ac2ca61
bcotl <shdw.synister@yahoo.com>
2020-02-09 17:50:53 -0800

    testing if music works

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 import { Helmet } from 'react-helmet'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
@@ -50,8 +51,10 @@ const TemplateWrapper = ({ children }) => {
       </Helmet>
       <Navbar />
       <div>{children}</div>
+      <ReactPlayer width="100%" height="450" playing
+        url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/984925351%3Fsecret_token%3Ds-YuWjK&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" />
       <Footer />
-    </div>
+    </div >
   )
 }
 
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -22,14 +22,14 @@ export const IndexPageTemplate = ({
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
-        backgroundPosition: `top left`,
+        // backgroundPosition: `top left`,
         backgroundAttachment: `fixed`,
       }}
     >
       <div
         style={{
           display: 'flex',
-          height: '150px',
+          height: '250px',
           lineHeight: '1',
           justifyContent: 'space-around',
           alignItems: 'left',
@@ -52,9 +52,9 @@ export const IndexPageTemplate = ({
         <h3
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+            // boxShadow:
+            //   'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+            // backgroundColor: 'rgb(255, 68, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
